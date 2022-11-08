9e9f4d1e4aa7c3d44e178948ed87c68dbf310aec
Ashish <102414529+Aashishweb@users.noreply.github.com>
2022-11-08 11:25:03 +0530

    Completed Login Page

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    
+    extend: {
+      colors : {
+        brightyellow: '#F4F1E9',
+        lightorange: '#FDC886',
+        darkorange: '#F1855D',
+        lightgray: '#767272'
+      },
+      fontFamily:{
+        Lato :['Lato']
+      }
+    },
+  },
+  plugins: [],
+}
